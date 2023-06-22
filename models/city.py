268af0dbd3332d17e38aaefaf66e794c23718a8f
WanjiruMuia <117749190+WanjiruMuia@users.noreply.github.com>
2023-06-22 15:01:56 +0300

    Update city.py

--- models/city.py
+++ models/city.py
@@ -1,23 +1,14 @@
-#!/usr/bin/python
-""" holds class City"""
-import models
-from models.base_model import BaseModel, Base
-from os import getenv
-import sqlalchemy
-from sqlalchemy import Column, String, ForeignKey
-from sqlalchemy.orm import relationship
+#!/usr/bin/python3
+"""
+City module
+"""
+from models.base_model import BaseModel
 
-class City(BaseModel, Base):
-    """Representation of city """
-    if models.storage_t == "db":
-        __tablename__ = 'cities'
-        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-        name = Column(String(128), nullable=False)
-        places = relationship("Place", backref="cities")
-    else:
-        state_id = ""
-        name = ""
 
-    def __init__(self, *args, **kwargs):
-        """initializes city"""
-        super().__init__(*args, **kwargs)
+class City(BaseModel):
+    """
+    Class City
+    """
+
+    state_id = ""
+    name = ""
