a61dda9087f87349d7a4601737a56298a461fb8b
WanjiruMuia <117749190+WanjiruMuia@users.noreply.github.com>
2023-06-22 14:59:58 +0300

    amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -1,20 +1,10 @@
-#!/usr/bin/python
-""" holds class Amenity"""
-import models
-from models.base_model import BaseModel, Base
-from os import getenv
-import sqlalchemy
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
+#!/usr/bin/python3
+"""
+This is amenity class that represents new amenities
+"""
+from models.base_model import BaseModel
 
-class Amenity(BaseModel, Base):
-    """Representation of Amenity """
-    if models.storage_t == 'db':
-        __tablename__ = 'amenities'
-        name = Column(String(128), nullable=False)
-    else:
-        name = ""
 
-    def __init__(self, *args, **kwargs):
-        """initializes Amenity"""
-        super().__init__(*args, **kwargs)
+class Amenity(BaseModel):
+    """ Amenity subclass that inherits from BaseModel """
+    name = ""
