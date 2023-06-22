110faa9a3912b9843a614bbf8a073069b854d5ad
WanjiruMuia <117749190+WanjiruMuia@users.noreply.github.com>
2023-06-22 15:04:54 +0300

    Update user.py

--- models/user.py
+++ models/user.py
@@ -1,28 +1,13 @@
 #!/usr/bin/python3
-""" holds class User"""
-import models
-from models.base_model import BaseModel, Base
-from os import getenv
-import sqlalchemy
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
+"""
+This is User class to represent new users
+"""
+from models.base_model import BaseModel
 
-class User(BaseModel, Base):
-    """Representation of a user """
-    if models.storage_t == 'db':
-        __tablename__ = 'users'
-        email = Column(String(128), nullable=False)
-        password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=True)
-        last_name = Column(String(128), nullable=True)
-        places = relationship("Place", backref="user")
-        reviews = relationship("Review", backref="user")
-    else:
-        email = ""
-        password = ""
-        first_name = ""
-        last_name = ""
 
-    def __init__(self, *args, **kwargs):
-        """initializes user"""
-        super().__init__(*args, **kwargs)
+class User(BaseModel):
+    """ User subclass that inherits from BaseModel """
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
