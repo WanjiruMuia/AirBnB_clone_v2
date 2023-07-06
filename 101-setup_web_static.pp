4e386e75df828d2f5647162b16fdd7958acd2757
WanjiruMuia <117749190+WanjiruMuia@users.noreply.github.com>
2023-07-06 13:04:05 +0300

    Create 101-setup_web_static.pp

---
+++ 101-setup_web_static.pp
@@ -0,0 +1,22 @@
+# Configures a web server for deployment of web_static.
+
+# Nginx configuration file
+$nginx_conf = "server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    add_header X-Served-By ${hostname};
+    root   /var/www/html;
+    index  index.html index.htm;
+    location /hbnb_static {
+        alias /data/web_static/current;
+        index index.html index.htm;
+    }
+    location /redirect_me {
+        return 301 https://th3-gr00t.tk;
+    }
+    error_page 404 /404.html;
+    location /404 {
+      root /var/www/html;
+      internal;
+    }
+}"
